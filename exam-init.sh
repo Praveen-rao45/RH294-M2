822d2222d171741019c860c2955a3254a3a699e6
Prithivraj Gankai R <prithivrajgankai@gmail.com>
2023-09-26 18:12:48 +0530

    Update exam-init.sh

--- exam-init.sh
+++ exam-init.sh
@@ -10,7 +10,7 @@ systemctl  restart httpd   > /dev/null 2>&1
 sudo -u student  mkdir /home/student/data
 sudo -u student  cd /home/student/data
 wget -P /home/student/data http://localhost/Rhce/mock2/inventory    > /dev/null 2>&1
-wget -P /home/student/data http:///localhost/mock2/ansible.cfg  > /dev/null 2>&1
+wget -P /home/student/data http:///localhost/Rhce/mock2/ansible.cfg  > /dev/null 2>&1
 sudo -u student  mkdir /home/student/data/collections
 sudo -u student  ansible-galaxy collection install  http://localhost/Rhce/ansible-posix-1.4.0.tar.gz -p /home/student/data/collections  > /dev/null 2>&1
 sudo -u student  ansible-galaxy collection install  http://localhost/Rhce/redhat-rhel_system_roles-1.0.0.tar.gz -p /home/student/data/collections  > /dev/null 2>&1
@@ -18,7 +18,7 @@ sudo -u student  ansible-galaxy collection install  http://localhost/Rhce/commun
 
 echo -ne '##############             (50%)\r'
 
-sudo -u student  wget -P /home/student/data http://content/Rhce/initialscripts.sh    > /dev/null 2>&1
+sudo -u student  wget -P /home/student/data http://localhost/Rhce/initialscripts.sh    > /dev/null 2>&1
 
 sudo -u student sh /home/student/data/initialscripts.sh > /dev/null 2>&1
 echo -ne '###########################(100%)\n'
