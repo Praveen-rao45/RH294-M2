0f6cad56e0238cb0990004210f1f4cdb49a98bae
Prithivraj Gankai R <prithivrajgankai@gmail.com>
2023-09-26 17:38:26 +0530

    Update initialscripts.sh

--- Rhce/initialscripts.sh
+++ Rhce/initialscripts.sh
@@ -1,2 +1,2 @@
-wget http://localhost/Rhce/admin.yml       > /dev/null 2>&1
-ansible-playbook admin.yml      > /dev/null 2>&1
+sudo -u student wget -P /home/student/data http://localhost/Rhce/admin.yml       > /dev/null 2>&1
+sudo -u student  ansible-playbook /home/student/data/admin.yml      > /dev/null 2>&1
